f2d3bf923511fa3f9b8c05caf4fea3c50dc39688
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-07-06 02:16:14 -0400

    add local storage for search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,31 @@
 var apiKey = "a98fb38cc5281d895fed558ea81f6eeb"
 var form = $("#form");
 var search = $("#search")
+var searchList =$(#"searchesList")
 
 // event listener for form submission
 form.on("submit", function(x) {
     x.preventDefault();
     // takes input value from the users submission
    var searchInput = search.val();
-   console.log(searchInput);
+//    console.log(searchInput);
 //    for when searching with no input 
    if (searchInput == "") {
     return;
    }
-})
 
+var searchHistory = [];
+
+// If search is a new unique one this will push it to the search history array
+if (!searchHistory.includes(searchInput)) {
+    searchHistory.push(searchInput);
+}
+// clears search window 
+search.val("");
+
+// add it to local Storage 
+localStorage.setItem("searchHistory", JSON.stringify(searchHistroy));
+
+fetchData();
+})
+ 
