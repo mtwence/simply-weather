30a0af6487abfacade3b99c3606edfc59e114108
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-07-06 01:33:58 -0400

    add jquery event listener for form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
+var apiKey = a98fb38cc5281d895fed558ea81f6eeb
 var form = $(#form);
+var search = $(#search)
+
+// event listener for form submission
+form.on("submit", function(x) {
+    x.preventDefault();
+    // takes input value from the users submission
+   var searchInput = search.val();
+//    for when searching with no input 
+   if (searchInput == "") {
+    return;
+   }
+})
