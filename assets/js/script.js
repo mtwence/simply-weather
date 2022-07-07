d777451ff30b4ca92565738f0902422c919d21a4
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-07-06 20:02:17 -0400

    work with tutor to remake search function work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,105 +1,95 @@
 var apiKey = "a98fb38cc5281d895fed558ea81f6eeb"
 var form = $("#form");
-var search = $("#search")
+var search = $("#search");
 var searchList = $("#searchesList");
+
+
 var searchHistory = [];
 
-
-
-
-    if (searchHistory.length > 0) {
-        // compare previous searches from local storage 
-        var priorSearches = localStorage.getItem("searchHistory", JSON.stringify(searchHistory));
-        searchHistory = JSON.parse(priorSearches);
+// Runs when form is submitted 
+form.on("submit", function (x) {
+    x.preventDefault();
+    var searchInput = search.val();
+    console.log(searchInput);
+    fetchCurrentWeather(searchInput);
+    //    for when searching with no input 
+    if (searchInput === "") {
+        return;
     }
-    // If search is a new unique one this will push it to the search history array
     if (!searchHistory.includes(searchInput)) {
         searchHistory.push(searchInput);
         localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-        console.log(searchHistory);
         search.val("");
     }
+
+// function to append searches to a list 
     var searchHistoryList = function (city) {
-
         var cityList = $("<li>")
-        cityList.addClass("prev-search");
-        cityList.text(city);
+        cityList.addClass("prev-search")
+        cityList.text(city)
+        cityList.on("click", function (x) {
+            x.preventDefault();
+            searchInput = x.target.innerHTML;
+            // console.log(x.target.innerHTML)
+           fetchCurrentWeather(searchInput);
+        });
         cityList.appendTo(searchList);
-
-    }
+    };
     searchHistoryList(searchInput);
-    // reset search field 
-    search.val("");
+});
+
+
+// if (searchHistory.length > 0) {
+//     // compare previous searches from local storage 
+//     var priorSearches = localStorage.getItem("searchHistory", JSON.stringify(searchHistory));
+//     searchSaved = JSON.parse(priorSearches);
+//     console.log(searchSaved);
+// }
+// // If search is a new unique one this will push it to the search history array
+// else {
+//     search.val("");
+    // ;
+// }
+// searchHistoryList(searchInput)
+
 
 // Function for search history to persist 
 var searchHistoryPersist = function () {
     var savedCity = localStorage.getItem("searchHistory");
-        savedCity = JSon.parse(savedCity)
-        if (savedCity == null) {
-    for ( var i = 0; i <searchHistory.length; i++) {
-        searchHistoryList(searchInput)
+    savedCity = JSON.parse(savedCity)
+    console.log(savedCity);
+    if (savedCity) {
+        for (var i = 0; i < savedCity.length; i++) {
+            searchHistoryList(savedCity[i])
         }
     }
 }
+searchHistoryPersist();
 
-// Fetch current weather data 
-var currentWeather = function fetchCurrentWeather(searchInput) {
-    // Turn city into lat & lon 
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&units=imperial&appid=" + apiKey)
-        // get response and turn it into json objects
-        .then(function (response) {
-            return response.json();
-        })
-        // Set variables for lat and lon responses 
-        .then(function (response) {
-            var cityLon = response.coord.lon;
-            var cityLat = response.coord.lat;
+// // Fetch current weather data 
+// function fetchCurrentWeather(searchInput) {
+//     // Turn city into lat & lon 
+//     fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&units=imperial&appid=" + apiKey)
+//         // get response and turn it into json objects
+//         .then(function (res) {
+//             // console.log(res.json());
+//             return res.json();
 
-            fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + response.coord.lat + "&lon=" + response.coord.lon +
-                "&exclude=hourly,minutely,alerts&units=imperial&appid=")
-        }
+//         }).then(function (data) {
+//             var cityLon = data[0].lon;
+//             var cityLat = data[0].lat;
 
-
-    
-}
-// Runs when form is submitted 
-form.on("submit", function (x) {
-    x.preventDefault();
-    // takes input value from the users submission
-    var searchInput = search.val();
-    //    for when searching with no input 
-    if (searchInput === "" || searchInput == null){
-        alert("Please search by city name.");
-        x.preventDefault();
-        
-    else{
-        curentWeather(searchInput);
-        forecast(searchInput);
-
-        }
-    }
-});
-        
+//             fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon +
+//                 "&exclude=hourly,minutely,alerts&units=imperial&appid=" + apiKey)
+//         .then(function(res) {
+//             return res.json();
+//         }).then(function(data){
+//             console.log(data);
+//         })
+//  })}
 
 
-// }
-
-// Append search to search history list
-
-
-
-// }
-
-// function searchList(search, i) {
-//     var search = searchHistory[i];
-//     var li = $("<li>").text(search);
-//     li.appendTo(searchList);
-//   };
-// searchList(searchInput, searchHistory.indexOf(searchInput))
-// // clears search window
-// search.val("");
-
-// // add to list by calling searchList function
-
-// })
+        // Set variables for lat and lon responses
+        // .then(function (data) {
+        //     console.log(data);
 
